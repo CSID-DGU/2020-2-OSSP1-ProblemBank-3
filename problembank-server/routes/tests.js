7dfc60ef738ca51166bf0023109c2bf67a51b3cb
kangtae1 <kangtaeone@gmail.com>
2020-11-22 01:38:04 +0900

    Fix some api and making others

--- problembank-server/routes/tests.js
+++ problembank-server/routes/tests.js
@@ -18,7 +18,7 @@ router.get('/alltestdata', async function(req, res) {
 })
 
 // 시험 문제 목록
-router.get('/testproblems', async function(req, res){
+router.get('/testproblems', async function(req, res) {
     const { test_id } = req.query
     try {
         let [rows] = await db.query(sql.tests.selectTestProblemsByTestId, [test_id])
@@ -26,7 +26,7 @@ router.get('/testproblems', async function(req, res){
         {
             let {problem_id} = rows[i]
             let [problem] = await db.query(sql.tests.selectTestProblemNameByProblemId, [problem_id])
-            rows[i]["name"] = problem;
+            rows[i]["name"] = problem[0].name;
         }
         res.status(200).send({
             result : true,
@@ -40,11 +40,10 @@ router.get('/testproblems', async function(req, res){
 })
 
 // 시험 문제 세부 정보
-router.get('/testproblemdata', async function(req, res){
+router.get('/testproblemdata', async function(req, res) {
     const { problem_id } = req.query
     try {
         let [rows] = await db.query(sql.tests.selectTestProblemContentsByProblemId, [problem_id])
-        console.log(rows)
         let [testcases] = await db.query(sql.tests.selectTestCaseByProblemId, [problem_id])
         let filterTestCase = testcases.map(testcase => ({
             input_exp: testcase.input_example, 
@@ -64,10 +63,15 @@ router.get('/testproblemdata', async function(req, res){
 })
 
 // 피드백 목록 출력
-router.get('/testfeedback', async function(req, res){
+router.get('/testfeedback', async function(req, res) {
     const { test_id } = req.query
     try {
-        let [rows] = await db.query(sql.tests.selectFeedBackByTestId, [test_id])
+        const [rows] = await db.query(sql.tests.selectFeedBackByTestId, [test_id])
+        for(let i = 0; i < rows.length; i++) {
+            const { author_id } = rows[i]
+            const [name] = await db.query(sql.tests.selectUserNameById, [author_id])
+            rows[i]["author_name"] = name[0].user_name;
+        }
         res.status(200).send({
             result : true,
             data: rows,
@@ -80,10 +84,15 @@ router.get('/testfeedback', async function(req, res){
 })
 
 //전체 응시자 결과 출력
-router.get('/adminresult', async function(req, res){
+router.get('/adminresult', async function(req, res) {
     const { test_id } = req.query
     try {
         let [rows] = await db.query(sql.tests.selectTestUsersByTestId, [test_id])
+        for(let i = 0; i < rows.length; i++) {
+            const { user_id } = rows[i]
+            const [name] = await db.query(sql.tests.selectUserNameById, [user_id])
+            rows[i]["user_name"] = name[0].user_name;
+        }
         res.status(200).send({
             result: true,
             data: rows,
@@ -95,10 +104,15 @@ router.get('/adminresult', async function(req, res){
 })
 
 //개인 시험 결과 출력
-router.get('/userresult', async function(req, res){
+router.get('/userresult', async function(req, res) {
     const { user_id } = req.query
     try {
         let [rows] = await db.query(sql.tests.selectTestUsersByUserId, [user_id])
+        for(let i = 0; i < rows.length; i++) {
+            const { test_id } = rows[i]
+            const [testname] = await db.query(sql.tests.selectTestNameByTestId, [test_id])
+            rows[i]["test_name"] = testname[0].name;
+        }
         res.status(200).send({
             result: true,
             data: rows,
@@ -107,6 +121,27 @@ router.get('/userresult', async function(req, res){
     } catch (error) {
         console.log("User Result Data" + error)
     }
+})
+
+// TODO:
+// 새 문제 생성 및 추가
+router.post('/createproblem', async function(req, res){
+    const {
+        name, content, input_cases, output_cases
+    } = req.body
+})
+
+// 기존 문제 추가
+router.post('/addproblem', async function(req, res) {
+    const { problem_ids } = req.body
+})
+
+// 테스트 생성
+router.post('/createtest', async function(req, res){
+    const {
+        name, content, is_exam, start, end, admin_id, subject_id,
+        problem_ids
+    } = req.body
 })
 
 module.exports = router;
--- problembank-server/sql/tests.js
+++ problembank-server/sql/tests.js
@@ -2,6 +2,7 @@ module.exports = {
 
     // SELECT
     selectTests: "SELECT * FROM problems.pb_tests",
+    selectTestNameByTestId: "SELECT name FROM problems.pb_tests WHERE id = ?",
     selectTestProblemsByTestId: "SELECT problem_id FROM problems.pb_test_problems WHERE test_id = ?",
     selectTestProblemContentsByProblemId: "SELECT * FROM problems.pb_test_problem_contents WHERE id = ?",
     selectTestProblemNameByProblemId: "SELECT name FROM problems.pb_test_problem_contents WHERE id = ?",
@@ -11,13 +12,16 @@ module.exports = {
     selectSubjectUsersBySubjectId: "SELECT * FROM problems.pb_subject_users WHERE subject_id = ?",
     selectTestUsersByTestId: "SELECT * FROM problems.pb_test_users WHERE test_id = ?",
     selectTestUsersByUserId: "SELECT * FROM problems.pb_test_users WHERE user_id = ?",
+    selectUserNameById: "SELECT user_name FROM problems.pb_users WHERE id = ?",
     
     // INSERT
-    insertTestContent: "INSERT INTO problems.pb_test_problem_contents (name, content, input, output) VALUES (?, ?, ?, ?)",
-    insertTest: "INSERT INTO problems.pb_test_problems (test_id, problem_id) VALUES (?, ?)",
+    insertTest: "INSERT INTO problems.pb_tests (name, content, is_exam, start, end, admin_id, subject_id) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    insertProblemIntoTest: "INSERT INTO problems.pb_test_problems (test_id, problem_id) VALUES (?, ?)",
+    insertProblem: "INSERT INTO problems.pb_test_problem_contents (name, content, input, output) VALUES (?, ?, ?, ?)",
+    insertTestCases: "INSERT INTO problems.pb_test_testcases (input_example, output_example, problem_id) VALUES (?, ?, ?)",
 
     // UPDATE
-    updateTestUserScoreByTestUserId: "UPDATE problems.pb_test_users SET score = ?, correct = ?, wrong = ?, applied = ? WHERE test_id = ? AND user_id = ?",
+    updateTestUserScoreByTestUserId: "UPDATE problems.pb_test_users SET score = ?, correct = ?, wrong = ?, applied = 1 WHERE test_id = ? AND user_id = ?",
 
 
     // DELETE

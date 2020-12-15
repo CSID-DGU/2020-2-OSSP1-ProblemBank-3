8f0a7b8e9d97f9d3a3d2acd26d182918ad79dc3a
서준원 <junwon@seojun-won-ui-MacBookPro.local>
2020-12-15 17:16:33 +0900

    login page edit

--- problembank-client/src/pages/LoginPage/index.js
+++ problembank-client/src/pages/LoginPage/index.js
@@ -1,16 +1,17 @@
-import React, { useState } from "react"
-import { Redirect } from "react-router-dom"
+import React, {useState} from "react"
+import {Redirect} from "react-router-dom"
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./LoginPage.css";
+import "./style.scss"
+import ManageTestLayout from "../../layouts/ManageTestLayout";
+import Text from "../../components/DesignComponent/Text";
 
-function LoginPage({ auth, login, location }) {
+function LoginPage({auth, login, location}) {
     const [id, setId] = useState("")
     const [password, setPassword] = useState("")
 
     const handleLogin = (event) => {
         event.preventDefault();
-        login({ id, password })
+        login({id, password})
         setId("")
         setPassword("")
     }
@@ -19,34 +20,39 @@ function LoginPage({ auth, login, location }) {
         return id.length > 0 && password.length > 0;
     }
 
-    const { from } = location.state || { from: { pathname: "/" } }
-    if (auth) return <Redirect to={from} />
+    const {from} = location.state || {from: {pathname: "/"}}
+    if (auth) return <Redirect to={from}/>
     return (
-            <div className="Login">
+        <div class="LoginPage">
+            <div class="input">
                 <Form onSubmit={handleLogin}>
-                    <Form.Group size="lg" controlId="id">
-                        <Form.Label>ID</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="id"
-                            value={id}
-                            onChange={(e) => setId(e.target.value)}
-                        />
+                    <Form.Group size="lg">
+                        <Form.Label>아이디</Form.Label>
+                        <div>
+                            <input
+                                class="inputArea"
+                                type="id"
+                                size="15"
+                                onChange={(e) => setId(e.target.value)}
+                            />
+                        </div>
                     </Form.Group>
-                    <Form.Group size="lg" controlId="password">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            autoFocus
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+                    <div class="margin"/>
+                    <Form.Group size="lg">
+                        <Form.Label>비밀번호</Form.Label>
+                        <div>
+                            <input
+                                class="inputArea"
+                                type="password"
+                                   size="15"
+                                   onChange={(e) => setId(e.target.value)}
+                            />
+                        </div>
                     </Form.Group>
-                    <Button block size="lg" type="submit" disabled={!validateForm()}>
-                        Login
-                    </Button>
+                    <button class="button" disabled={!validateForm()}> 로그인</button>
                 </Form>
             </div>
+        </div>
     )
 }
 
